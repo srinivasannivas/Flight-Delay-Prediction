7ebac2322e4165f43215db07774cc68ccc93b3a5
Srinivasan C <132070116+srinivasannivas@users.noreply.github.com>
2025-04-07 10:32:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -63,5 +63,5 @@ def predict():
         return render_template("index.html", error=str(e))
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8000)) 
     app.run(debug=False, host="0.0.0.0", port=port)
