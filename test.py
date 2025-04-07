88536d4f01322232604b34933e3e9a554079d69f
Srinivasan C <132070116+srinivasannivas@users.noreply.github.com>
2025-04-07 14:35:28 +0530

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import joblib
-
-# Load your encoders
-encoders = joblib.load("encoders.pkl")
-
-# Print valid label values for each feature
-for name, enc in encoders.items():
-    print(f"{name}: {list(enc.classes_)}")
